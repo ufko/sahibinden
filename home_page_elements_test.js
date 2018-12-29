3a04ff466cd52806576a4ab316512517a43e46e2
ufuk <ufukozcelik8@gmail.com>
2018-12-29 15:52:51 +0300

    Add tests to check if left menu titles exist
    Left menu titles are Vasıta, below Vasıta Otomobil, Arazi, SUV & Pick-up, Motosiklet.

--- home_page_elements_test.js
+++ home_page_elements_test.js
@@ -4,12 +4,20 @@ Feature('sahibinden.com Home Page');
 Scenario('Page elements exist', (I) => {
     I.amOnPage('https://www.sahibinden.com');
 
+    I.amOnPage('https://www.sahibinden.com');
+
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/p/a[contains(@class, \'logo\')]'});
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/form/input[@id=\'searchText\']'});
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/form/a[contains(text(),\'Detaylı Arama\')]'});
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/ul/li[contains(@class, \'login-text\')]/a[contains(text(),\'Giriş Yap\')]'});
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/ul/li[contains(@class, \'register-text\')]/a[contains(text(),\'Üye Ol\')]'});
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/ul/li[contains(@class, \'new-classified\')]/a[contains(text(),\'Ücretsiz* İlan Ver\')]'});
+
+    I.seeElement({xpath:'/html/body//ul[contains(@class, \'categories-left-menu\')]/li/a[contains(text(), \'Vasıta\')]'});
+    I.seeElement({xpath:'/html/body//ul[contains(@class, \'categories-left-menu\')]/li/a[contains(text(), \'Vasıta\')]/../ul/li/a[contains(text(), \'Otomobil\')]'});
+    I.seeElement({xpath:'/html/body//ul[contains(@class, \'categories-left-menu\')]/li/a[contains(text(), \'Vasıta\')]/../ul/li/a[contains(text(), \'Arazi, SUV & Pick-up\')]'});
+    I.seeElement({xpath:'/html/body//ul[contains(@class, \'categories-left-menu\')]/li/a[contains(text(), \'Vasıta\')]/../ul/li/a[contains(text(), \'Motosiklet\')]'});
+
     I.seeElement({xpath:'/html/body//div[contains(@class, \'showcase\')]/h3[contains(., \'Anasayfa Vitrini\')]'});
     I.seeNumberOfElements({xpath:'/html/body//div[contains(@class, \'showcase\')]/ul/li'}, 56);
 
