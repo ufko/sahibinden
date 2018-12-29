7b07559aa39e8fef4acd2507e6f7c290908c4c55
ufuk <ufukozcelik8@gmail.com>
2018-12-29 15:51:37 +0300

    Add tests for elements in the middle of the page and rename the scenario

--- home_page_elements_test.js
+++ home_page_elements_test.js
@@ -1,12 +1,16 @@
 
 Feature('sahibinden.com Home Page');
 
-Scenario('Header elements exist', (I) => {
+Scenario('Page elements exist', (I) => {
     I.amOnPage('https://www.sahibinden.com');
+
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/p/a[contains(@class, \'logo\')]'});
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/form/input[@id=\'searchText\']'});
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/form/a[contains(text(),\'Detaylı Arama\')]'});
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/ul/li[contains(@class, \'login-text\')]/a[contains(text(),\'Giriş Yap\')]'});
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/ul/li[contains(@class, \'register-text\')]/a[contains(text(),\'Üye Ol\')]'});
     I.seeElement({xpath:'/html/body//div[contains(@class, \'header\')]/ul/li[contains(@class, \'new-classified\')]/a[contains(text(),\'Ücretsiz* İlan Ver\')]'});
+    I.seeElement({xpath:'/html/body//div[contains(@class, \'showcase\')]/h3[contains(., \'Anasayfa Vitrini\')]'});
+    I.seeNumberOfElements({xpath:'/html/body//div[contains(@class, \'showcase\')]/ul/li'}, 56);
+
 });
